840a1382e93091ec0f635c80ff4179b0a4b86989
root <root@DESKTOP-SH55I7R>
2024-08-14 15:00:19 +0200

    save fuction

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int shell_execute(char **args);
+void prompt_main(char **input_line);
+int dash_exit(void);
+char *read_line();
+char **split_token(char *input_line);
+#endif
