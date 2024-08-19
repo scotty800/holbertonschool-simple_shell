95cee230365afbef33373c655419d9e97c28a867
scotty800 <root@DESKTOP-SH55I7R>
2024-08-19 16:21:03 +0200

    fix betty

--- _strdup.c
+++ _strdup.c
@@ -11,14 +11,15 @@ char *_strdup(const char *str)
 {
 	size_t len;
 	char *dup;
+
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	len = strlen(str) + 1;
 	dup = malloc(len);
 
 	if (dup == NULL)
-		return NULL;
+		return (NULL);
 
 	memcpy(dup, str, len);
 	return (dup);
