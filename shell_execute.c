c6b72170efc25ae2699129009247282169829ef5
scotty800 <root@DESKTOP-SH55I7R>
2024-08-22 11:27:57 +0200

    delete function

--- shell_execute.c
+++ shell_execute.c
@@ -1,19 +1,6 @@
 #include "shell.h"
 
 /**
- * _cmp - Compares the first argument with specific commands and executes
- * corresponding functions.
- * @args: Array of strings containing command and arguments.
- */
-void _cmp(char **args)
-{
-	if (_strcmp(args[0], "exit") == 0)
-		(dash_exit());
-
-	else if (_strcmp(args[0], "env") == 0)
-		(shell_env());
-}
-/**
  * shell_execute - Executes a command based on user input.
  * @args: An array of command arguments Forks a child process to execute
  * is "exit", the shell terminates. Handles errors by printing error messages
@@ -25,7 +12,10 @@ int shell_execute(char **args)
 	int status;
 	char *cmd_path = NULL;
 
-	_cmp(args);
+	if (_strcmp(args[0], "exit") == 0)
+		(dash_exit());
+	else if (_strcmp(args[0], "env") == 0)
+		(shell_env());
 	if (args[0][0] == '/' || args[0][0] == '.')
 		cmd_path = args[0];
 	else
@@ -57,7 +47,5 @@ int shell_execute(char **args)
 		if (waitpid(cpid, &status, WUNTRACED) == -1)
 			perror("waitpid");
 	}
-	if (cmd_path != args[0])
-		free(cmd_path);
 	return (1);
 }
